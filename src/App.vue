df511166baba0b61a994f049360cc2f5ae3fb08e
Samuel Stoltenberg <samuel@taubyte.com>
2022-09-19 04:06:50 -0500

    Basic site idea shown

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TableView />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableView from "./components/TableView.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TableView,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TableView.vue
@@ -0,0 +1,68 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "TableView",
+  data() {
+    return {
+      host: "http://hal.computers.com:9090",
+      /** @type Array<{
+       * protocol: string,
+       * command: string,
+       * data: string,
+       * from: string,
+       * to: string
+       * } */
+      data: [],
+    };
+  },
+
+  methods: {
+    async ping() {
+      let response = await axios.get(this.host + "/ping");
+
+      // only add if unique, and increment non-unique
+      let found = false;
+      for (let row of this.data) {
+        if (row.to == response.data.to && row.from == response.data.from) {
+          found = true;
+          row.count += 1;
+        }
+      }
+
+      if (!found) {
+        response.data.count = 1;
+        this.data.push(response.data);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <button @click="ping">Ping</button>
+  <table>
+    <thead>
+      <tr>
+        <th>Protocol</th>
+        <th>Command</th>
+        <th>Data</th>
+        <th>From</th>
+        <th>To</th>
+        <th>Count</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in data" :key="item">
+        <td>{{ item.protocol }}</td>
+        <td>{{ item.command }}</td>
+        <td>{{ item.data }}</td>
+        <td>{{ item.from }}</td>
+        <td>{{ item.to }}</td>
+        <td>{{ item.count }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
